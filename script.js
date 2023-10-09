ba3ad31b8ec709532d416a36d8f319e491b04a05
Mayankkatheriya <mayankkatheriya4@gmail.com>
2023-10-09 23:32:59 +0530

    currency feature added

--- script.js
+++ script.js
@@ -2,13 +2,28 @@ let btn = document.querySelector('button.calc');
 let amount = document.querySelector("#tip");
 let service = document.querySelector(".service")
 let people = document.querySelector("#people")
+let currency = document.querySelector(".currency")
 let tipBox = document.querySelector(".total-tip")
 let tipDisplay = document.querySelector(".total-tip h1")
 btn.addEventListener("click", function(){
     let amt = Number(amount.value);
     let serv = Number(service.value);
     let ppl = Number(people.value);
-    let totalTip = (amt*serv)/ppl;
-    tipDisplay.innerText = `₹ ` + totalTip.toFixed(2) + ((ppl>1) ? " Each" : "");
+    let curr = Number(currency.value);
+    let totalTip = (amt*serv*curr)/ppl;
+    let sign = '₹ '
+    if(curr == 0.044){
+        sign = 'د.إ‎ '
+    }
+    else if(curr == 0.012){
+        sign = '$ '
+    }
+    else if(curr == 0.011){
+        sign = '€ '
+    }
+    else if(curr == 0.68){
+        sign = '₱ '
+    }
+    tipDisplay.innerText = sign + totalTip.toFixed(3) + ((ppl>1) ? " Each" : "");
     tipBox.style.display = "block"
 })
