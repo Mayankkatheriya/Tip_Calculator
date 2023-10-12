a6fd3a6b2d94e49814b1f8a798d67019c56fac7b
Mayankkatheriya <mayankkatheriya4@gmail.com>
2023-10-12 13:44:20 +0530

    linkedIn Added

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 let btn = document.querySelector('button.calc');
 let amount = document.querySelector("#tip");
-let service = document.querySelector(".service")
-let people = document.querySelector("#people")
-let currency = document.querySelector(".currency")
-let tipBox = document.querySelector(".total-tip")
-let tipDisplay = document.querySelector(".total-tip h1")
-btn.addEventListener("click", function(){
-    let amt = Number(amount.value);
-    let serv = Number(service.value);
-    let ppl = 1;
-    ppl = Number(people.value);
-    curr = Number(currency.value);
-    let totalTip = (amt*serv*curr)/ppl;
-    let sign = '₹ '
-    if(curr == 0.044){
-        sign = 'د.إ‎ '
+let service = document.querySelector(".service");
+let people = document.querySelector("#people");
+let currency = document.querySelector(".currency");
+let tipBox = document.querySelector(".total-tip");
+let tipDisplay = document.querySelector(".total-tip h1");
+
+btn.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const amt = Number(amount.value);
+    const serv = Number(service.value);
+    const ppl = Number(people.value);
+    const curr = Number(currency.value);
+
+    let sign = '₹ ';
+    if (curr === 0.044) {
+        sign = 'د.إ‎ ';
+    } else if (curr === 0.012) {
+        sign = '$ ';
+    } else if (curr === 0.011) {
+        sign = '€ ';
+    } else if (curr === 0.68) {
+        sign = '₱ ';
     }
-    else if(curr == 0.012){
-        sign = '$ '
-    }
-    else if(curr == 0.011){
-        sign = '€ '
-    }
-    else if(curr == 0.68){
-        sign = '₱ '
-    }
-    tipDisplay.innerText = sign + totalTip.toFixed(3) + ((ppl>1) ? " Each" : "");
-    tipBox.style.display = "block"
-})
+
+    const totalTip = (amt * serv * curr) / ppl;
+    tipDisplay.innerText = `${sign}${totalTip.toFixed(3)}${ppl > 1 ? " Each" : ""}`;
+    tipBox.style.display = "block";
+});
